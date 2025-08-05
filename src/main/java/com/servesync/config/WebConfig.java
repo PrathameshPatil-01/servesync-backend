04a34220e2a32f48f968f08e3b8636e18caa0331
Shubham Sakande <initiator2210@gmail.com>
2025-08-05 16:25:56 +0530

    m

--- src/main/java/com/servesync/config/WebConfig.java
+++ src/main/java/com/servesync/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all paths
-                .allowedOrigins("http://localhost:5173") // Frontend origin
+                .allowedOrigins("http://localhost:8080") // Frontend origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
