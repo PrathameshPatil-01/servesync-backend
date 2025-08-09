a940a4aa1549fba525980c8a1fb23678f89537c1
Shubham Sakande <initiator2210@gmail.com>
2025-08-09 10:54:15 +0530

    service changes

---
+++ src/main/java/com/servesync/repository/service/ServiceRepository.java
@@ -0,0 +1,16 @@
+package com.servesync.repository.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.servesync.entity.service.Service;
+
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+
+	Optional<Service> findByServiceName(String serviceName);
+	List<Service> findByIsActiveTrue();
+	
+}
---
+++ src/main/java/com/servesync/service/service/ServiceService.java
@@ -0,0 +1,16 @@
+package com.servesync.service.service;
+
+import java.security.Provider.Service;
+import java.util.List;
+import java.util.Optional;
+
+import com.servesync.dto.service.AddServiceDto;
+import com.servesync.dto.service.ServiceDto;
+
+
+public interface ServiceService {
+
+	
+	List<ServiceDto> getAllServices();
+	String addService(AddServiceDto serviceDto);
+}
